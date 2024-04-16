acec239d33237ef04a110d1993c72405bbc15698
luisaferreirass <152530880+luisaferreirass@users.noreply.github.com>
2024-04-16 12:32:00 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+let number1 = Number(prompt("Dígite o primeiro número"))
+let number2 = Number(prompt("Dígite o segundo número"))
+
+let soma = number1 + number2
+let subtração = number1 - number2
+let multiplicação = number1 * number2
+let divisão = number1 / number2
+let resto = number1 % number2
+
+alert(`A soma dos dois números é: ${soma}`)
+alert(`A subtração dos dois números é: ${subtração}`)
+alert(`A multiplicação dos dois números é: ${multiplicação}`)
+alert(`A divisão dos dois números é: ${divisão}`)
+alert(`O resto da divisão dos dois números é: ${resto}`)
+
+
